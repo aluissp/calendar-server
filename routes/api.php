9a8010edbc4b640ebf4206f0c6c54e58f4c56787
Luis Perugachi <luisprg05@gmail.com>
2023-01-03 23:12:51 -0500

    Auth controller created

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+  return $request->user();
+});
 // Public routes
+Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+
+Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
